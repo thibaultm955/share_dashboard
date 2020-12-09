5ae47a2d9e7c12a6049d5b385e7c7eb93ada9824
thibault mambour <thibault.mambour@protonmail.com>
2020-12-09 17:36:27 +0100

    updates

--- app/models/share_to_portfolio.rb
+++ app/models/share_to_portfolio.rb
@@ -2,6 +2,6 @@ class ShareToPortfolio < ApplicationRecord
   belongs_to :portfolio
   belongs_to :share
 
-  validates_uniqueness_of :share_id, message: "Not cool bro"
+  # validates_uniqueness_of :share_id, message: "Not cool bro"
 
 end
