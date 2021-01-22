88a890a1919d9501039baef8a7ee8bdc4d7b2ad6
thibault mambour <thibault.mambour@protonmail.com>
2021-01-23 00:37:44 +0100

    europe

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,9 +54,9 @@ industries.each do |industry|
     end
 end
 # Europe
-# urls = CSV.parse(File.read("cleaned_url.csv"), headers: true)
+urls = CSV.parse(File.read("cleaned_url.csv"), headers: true)
 # Nasdaq
-urls = CSV.parse(File.read("nasdaq_url.csv"), headers: true)
+#urls = CSV.parse(File.read("nasdaq_url.csv"), headers: true)
 
 time = Time.now
 date_today = time.strftime("%d/%m/%Y")
@@ -66,7 +66,7 @@ urls.each do |url|
     url_0 = url
 
     # Europe
-    # url = url[0]
+    url = url[0]
     # Nasdaq
     url = url[1]
     
